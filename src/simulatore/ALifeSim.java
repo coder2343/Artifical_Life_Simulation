9a372e30100ccf63048e60d2e0626a5c76e65c74
Liam Walsh <liamspencerwalsh@icloud.com>
2021-12-05 17:33:49 -0600

    add more items to population class

--- src/simulatore/ALifeSim.java
+++ src/simulatore/ALifeSim.java
@@ -1,6 +1,9 @@
 package simulatore;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
 
 public class ALifeSim {
 
@@ -8,22 +11,41 @@ public class ALifeSim {
 	/**
 	 * @param args <#/iterations> <#/cooperators> <#/defectors> <#/partial cooperators> 
 	 */
-	public void main(String args[]) {
-		int iterations, cooperators, defectors, partialCooperators; 
-		
+	public static void main(String args[]) {
+		Integer iterations =0, cooperators = 0, defectors=0, partialCooperators =0;
+		HashMap<String, Integer> inputTable = new HashMap<>();
 		try {
 			iterations = Integer.parseInt(args[0]);
-			cooperators = Integer.parseInt(args[0]);
-			defectors = Integer.parseInt(args[0]);
-			partialCooperators = Integer.parseInt(args[0]);
+			cooperators = Integer.parseInt(args[1]);
+			System.out.println(cooperators);
+			defectors = Integer.parseInt(args[2]);
+			partialCooperators = Integer.parseInt(args[3]);
 
 		}
 		catch(NumberFormatException e){
 			
 		}
+		inputTable.put("Cooperators", cooperators);
+		inputTable.put("Defectors", defectors);
+		inputTable.put("partialCooperators", partialCooperators);
 		
+		Population testPopulation = new Population(inputTable);
+
 		// implemmenting population
-		
+		for(int i= 0; i<100;i++) {
+			testPopulation.update();
+			
+		}
+		Map<String,Integer> expirmentOutput = testPopulation.getPopulationCounts();
+		System.out.println("After: " + iterations + " iterations" );
+		for(Map.Entry<String,Integer> entry : expirmentOutput.entrySet()) {
+			System.out.println(entry.getKey() + " " + entry.getValue());
+		}
+		System.out.println("mean cooperation proabability =" + " " + testPopulation.calcuateCooperationMean());
+		System.out.println("mean defector cooperation proabability =" + " " + testPopulation.getMeanDefectorCooperation());
+		System.out.println("mean partial  cooperator cooperation proabability =" + " " + testPopulation.getMeanPartialCooperatorCooperation());
+		System.out.println("mean  cooperator cooperation proabability =" + " " + testPopulation.getMeanCooperatorCooperation());
+
 		
 	}
 }
--- src/simulatore/Population.java
+++ src/simulatore/Population.java
@@ -1,15 +1,23 @@
 package simulatore;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 /**
  * @author walsl
  *SYntax error.
  */
 public class Population {
-	private Map<String, Integer> populationMap;
-	
-	
+	//private HashMap<String, Integer> populationMap;
+
+	// create types of organisms as private feilds
+	private ArrayList<Organism> lifeForms ;
+	private Integer numberCooperator;
+	private Integer numberDefector;
+	private Integer numberPartialCooperator;
+
 	/**
 	 * @param counts
 	 * Constructs a population of organisms dictated by the given set of pairs 
@@ -17,11 +25,44 @@ public class Population {
 	 *  Organisms not mentioned in the mapping do not appear in the population. 
 	 *  Throws an IllegalArgumentException if the mapping mentions organism types that do not exist in the program.
 	 */
-	public Population(Map<String, Integer> counts) {
-		populationMap = counts;
-		
+
+
+	public Population(HashMap<String, Integer> counts) {
+		//populationMap = counts;
+		lifeForms = new ArrayList<>();
+		//HashMap<String, Integer> entry;
+		for(Map.Entry<String,Integer> entry : counts.entrySet()) {
+			if( entry.getKey().equals("Cooperators")){
+				//System.out.println()
+				numberCooperator = entry.getValue();
+
+			}
+			else if( entry.getKey().equals("Defectors")){
+				numberDefector = entry.getValue();
+
+			}
+			else if( entry.getKey().equals("partialCooperators")){
+				numberPartialCooperator = entry.getValue();
+
+			}
+
+
+		}
+		//System.out.println(numberCooperator);
+
+		for(int i =0; i<numberCooperator; i++  ) {
+			lifeForms.add(new Cooperator());
+		}
+		for(int i =0; i<numberDefector; i++  ) {
+			lifeForms.add(new Defector());
+		}
+		for(int i =0; i<numberPartialCooperator; i++  ) {
+			lifeForms.add(new PartialCooperator());
+		}
+
+
 	}
-	
+
 	/**
 	 * oops through all the organisms in the population and (
 	 * 1) updates them (by calling their update method), 
@@ -35,7 +76,59 @@ public class Population {
 	 *    		DOES THAT mean any organism will be replaced or organism of some other type
 	 */
 	public void update() {
-		
+		Random replace = new Random();
+		int replaceIndex = replace.nextInt(lifeForms.size());
+		int replaceType = replace.nextInt(2);
+		for(int i = 0; i <lifeForms.size(); i++) {
+			lifeForms.get(i).update();
+			if(lifeForms.get(i).cooperates()&& (lifeForms.get(i).getEnergy())>=10) {
+				if(lifeForms.get(i).GetType().equals("Cooperator")) {
+					if(replaceType ==1) {
+						lifeForms.set(replaceIndex, new PartialCooperator());
+					}
+					else {
+						lifeForms.set(replaceIndex, new Defector());
+
+					}
+
+				}
+				else if(lifeForms.get(i).GetType().equals("Defector")){
+					if(replaceType ==1) {
+						lifeForms.set(replaceIndex, new PartialCooperator());
+					}
+					else {
+						lifeForms.set(replaceIndex, new Cooperator());
+
+					}
+
+				}
+				else if(lifeForms.get(i).GetType().equals("PartialCooperator")){
+					if(replaceType ==1) {
+						lifeForms.set(i, new Cooperator());
+
+					}
+					else {
+						lifeForms.set(i, new Defector());
+					}
+				}
+			}
+		}
+	}
+
+	/**
+	 * @return calculates the mean cooperation probability of all the organisms 
+	 * in the population葉he average of 
+	 * the cooperation probabilities of all the organisms in the population.
+	 */
+	public double calcuateCooperationMean() {
+		double cooperationProbability =0; 
+		double sumProabilites = 0;
+		for (int i =0 ; i <lifeForms.size(); i++){
+			sumProabilites +=lifeForms.get(i).getCooperationProbability();
+
+		}
+		cooperationProbability = sumProabilites/ lifeForms.size();
+		return cooperationProbability;		
 	}
 	
 	/**
@@ -43,24 +136,82 @@ public class Population {
 	 * in the population葉he average of 
 	 * the cooperation probabilities of all the organisms in the population.
 	 */
-	public double calcuateCooperationMean() {
-		int cooperatorNumber = populationMap.get("Cooperator");
-		int defectorNumber = populationMap.get("Defector");
-		int  partialCooperatorNumber  = populationMap.get("PartialCooperator");
-		
-		return ( (double) cooperatorNumber +
+	public double getMeanCooperatorCooperation() {
+		double cooperationProbability =0; 
+		double sumProabilites = 0;
+		for (int i =0 ; i <lifeForms.size(); i++){
+			if(lifeForms.get(i).GetType().equals("Cooperator")) {
+			sumProabilites +=lifeForms.get(i).getCooperationProbability();
+			}
 
-
-		
+		}
+		cooperationProbability = sumProabilites/ lifeForms.size();
+		return cooperationProbability;		
 	}
-	
+
+	/**
+	 * @return calculates the mean cooperation probability of all the organisms 
+	 * in the population葉he average of 
+	 * the cooperation probabilities of all the organisms in the population.
+	 */
+	public double getMeanPartialCooperatorCooperation() {
+		double cooperationProbability =0; 
+		double sumProabilites = 0;
+		for (int i =0 ; i <lifeForms.size(); i++){
+			if(lifeForms.get(i).GetType().equals("PartialCooperator")) {
+			sumProabilites +=lifeForms.get(i).getCooperationProbability();
+			}
+
+		}
+		cooperationProbability = sumProabilites/ lifeForms.size();
+		return cooperationProbability;		
+	}
+	/**
+	 * @return calculates the mean cooperation probability of all the organisms 
+	 * in the population葉he average of 
+	 * the cooperation probabilities of all the organisms in the population.
+	 */
+	public double getMeanDefectorCooperation() {
+		double cooperationProbability =0; 
+		double sumProabilites = 0;
+		for (int i =0 ; i <lifeForms.size(); i++){
+			if(lifeForms.get(i).GetType().equals("Defector")) {
+
+			sumProabilites +=lifeForms.get(i).getCooperationProbability();
+			}
+		}
+		cooperationProbability = sumProabilites/ lifeForms.size();
+		return cooperationProbability;		
+	}
+
 	/**
 	 * @return the counts of all the organisms in the population.
 	 */
 	public Map<String, Integer> getPopulationCounts(){
-		int cooperatorNumber = populationMap.get("Cooperator");
-		int defectorNumber = populationMap.get("Defector");
-		int  partialCooperatorNumber  = populationMap.get("PartialCooperator");
-		return cooperatorNumber+ defectorNumber +partialCooperatorNumber;
+		int cooperatorNumber =0;
+		int defectorNumber =0; 
+		int  partialCooperatorNumber  =0;
+		HashMap <String, Integer>resultMap  = new HashMap<>();
+		for(int i =0; i< lifeForms.size(); i++) {
+			if(lifeForms.get(i).GetType().equals("Cooperator")){
+				cooperatorNumber += 1;
+			}
+			else if( lifeForms.get(i).GetType().equals("Defector")){
+				defectorNumber +=1 ;
+			}
+			else if(lifeForms.get(i).GetType().equals("PartialCooperator")){
+				partialCooperatorNumber +=1;
+
+			}
+
+
+		}
+		resultMap.put("Cooperator", cooperatorNumber);
+		resultMap.put("Defector", defectorNumber);
+		resultMap.put("PartialCooperator", partialCooperatorNumber);
+
+		return resultMap;
 	}
+
 }
+
